c475411e1d5f969e8ac991d97e6e89fc8bb8b1a0
Tom Care <tcare@microsoft.com>
2020-04-10 12:30:05 -0700

    Try new CI image

--- ml_service/util/create_scoring_image.sh
+++ ml_service/util/create_scoring_image.sh
@@ -13,5 +13,5 @@ docker run \
   -e WORKSPACE_NAME=$WORKSPACE_NAME \
   -e MODEL_NAME=$MODEL_NAME \
   -e IMAGE_NAME=$IMAGE_NAME \
-  mcr.microsoft.com/mlops/python:latest \
+  mcr.microsoft.com/mlops/python:tcare-test \
   bash -c "cd /mlops/ && python ml_service/util/create_scoring_image.py"
